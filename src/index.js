0d60c7230ec6567bd41493b577ce3ad1bb112522
rado88x <rado88x@gmail.com>
2020-08-09 13:35:45 +0300

    server settings

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Application from './app'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
